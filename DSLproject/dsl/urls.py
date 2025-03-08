1622e5cb4d3f89442435aefde74ad7975fe8374b
DisCedric2 <dishantbansod210@gmail.com>
2025-03-08 23:39:17 +0530

    3rd commit

--- DSLproject/dsl/urls.py
+++ DSLproject/dsl/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.DSLhome, name='DSLhome'),
+    path("DSLhome/",views.DSLhome, name='DSLhome'),
     path("DSLsignup/",views.DSLsignup, name='DSLsignup'),
     path("DSLsignin/",views.DSLsignin, name='DSLsignin'),
     path("DSLaboutus/",views.DSLaboutus, name='DSLaboutus'),
