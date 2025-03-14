6e012d294d735cae6348a38f3229763adbe955f3
DisCedric2 <dishantbansod210@gmail.com>
2025-03-15 00:51:41 +0530

    7th one bro

--- DSLproject/dsl/urls.py
+++ DSLproject/dsl/urls.py
@@ -5,14 +5,14 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("",views.DSLhome, name='DSLhome'),
     path("DSLhome/",views.DSLhome, name='DSLhome'),
-    path("DSLsignup/",views.DSLsignup, name='DSLsignup'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='DSLhome'), name='logout'),
     path("DSLsignin/",views.DSLsignin, name='DSLsignin'),
+    path("DSLsignup/",views.DSLsignup, name='DSLsignup'),
+    path('logout_user/', views.logout_user, name='logout_user'),
     path("DSLaboutus/",views.DSLaboutus, name='DSLaboutus'),
     path("DSLcontactus/",views.DSLcontactus, name='DSLcontactus'),
     path('DSLsong/<int:song_id>/', views.DSLsong, name='DSLsong'),
     path('DSLupload/', views.DSLupload, name='DSLupload'),
     path("premium/", views.create_payment, name="create_payment"),
     path("premium/success/", views.payment_success, name="payment_success"),
-
+    
 ]
--- DSLproject/dsl/views.py
+++ DSLproject/dsl/views.py
@@ -1,12 +1,127 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.conf import settings
-from .models import AddSong
+from .models import AddSong, UpSong
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
+def index(req):
+    allsongs = AddSong.objects.all()
+    context = {"allsongs" : allsongs}
+    return render(req, "DSLhome.html", context)
+
+def validate_password(password) :
+    # check for min length
+    if len(password) < 7 :
+        raise ValidationError("Password must be atleast 7 characters long :/")
+    
+    # check for max length
+    if len(password) > 99:
+        raise ValidationError("Password too long try to fit it under 99 characters :/")
+    
+    # Initialsing flags for char checks
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    has_special = False
+    special_characters = "!@#$%^&*~<>?"
+
+    # check for char variety
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+        elif char in special_characters:
+            has_special = True
+
+    if not has_upper:
+        raise ValidationError("Password must contain atleast one uppercase letter :/")
+    if not has_lower:
+        raise ValidationError("Password must contain atleast one lowercase letter :/")
+    if not has_digit:
+        raise ValidationError("Password must contain atleast one digit :/")
+    if not has_special:
+        raise ValidationError("Password must contain atleast one special character (eg., !@#$%^&*~<>?) :/")
+
+    # check for commomn passwords
+    common_pass = ["password", "1234567", "Qwertyu", "abcd1234"]
+    if password in common_pass:
+        raise ValidationError("Entered Password is too common :( Please enter unique one :)")
+    
+def signup(req):
+    print(req.method)
+    context = {}
+    if req.method == "GET":
+        return render(req, "DSLsignup.html")
+    else:
+        print(req.method)
+        uname = req.POST["uname"]
+        uemail = req.POST["email"]
+        upass = req.POST["upass"]
+        ucpass = req.POST["ucpass"]
+        print(uname,uemail , upass, ucpass)
+
+        try:
+            validate_password(upass)
+        except ValidationError as e:
+            context["errmsg"] = str(e)
+            return render(req, "DSLsignup.html", context)
+        
+        if uname == "" or upass == "" or ucpass == "":
+            context["errmsg"] = "Field cannot be empty :("
+            return render(req, "DSLsignup.html", context)
+        elif upass != ucpass:
+            context["errmsg"] = "Password and Confirm Passwrod aint matching dude :("
+            return render(req, "DSLsignup.html", context)
+        elif uname.isdigit():
+            context["errmsg"] = "Username cannot be only Number smarty :("
+            return render(req, "DSLsignup.html", context)
+        elif upass == uname:
+            context["errmsg"] = "Password cannot be same as Username common dude u r lazy :("
+            return render(req, "DSLsignup.html", context)
+        else:
+            try:
+                userdata = User.object.create(username=uname, password=upass)
+                userdata.set_password(upass)
+                userdata.save()
+                print(User.objects.all())
+                return redirect("DSLsignin")
+            except:
+                print("User already exists :?")
+                context["errmsg"] = "User already exists :?"
+                return render(req, "DSLsignup.html", context)
+
+def signin(req):
+    if req.method == "POST":
+        uname = req.POST["uname"]
+        upass = req.POST["upass"]
+        context = {}
+        if uname == "" or upass == "":
+            context["errmsg"] = "Field cant be empty :("
+            return render(req, "signin.html", context)
+        else:
+            userdata = authenticate(username=uname, password=upass)
+            print(req.user.password)
+            if userdata is not None:
+                login(req, userdata)
+                return redirect("/")
+            else:
+                context["errmsg"] = "Invalid username and password :("
+                return render(req, "DSLsignup.html", context)
+    else:
+        return render(req, "DSLsignin.html")
+
+def logout_user(req):
+    logout(req)
+    messages.success(req, "You have successfully logged out :(")
+    return redirect("/")
+'''
 def login_user(request):
     if request.method == "POST":
         username = request.POST.get("username")
@@ -23,11 +138,7 @@ def login_user(request):
             return redirect("DSLsignin")
     
     return render(request, "DSLsignin.html")
-
-def logout_user(req):
-    logout(req)
-    messages.success(req, "You have successfully logged out.")
-    return redirect("DSLhome.html")
+'''
 
 
 print("welcome")
